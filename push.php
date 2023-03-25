f6df6655bb033f0ee562ff26448ce50b8a773275
rmens <r.mens@me.com>
2023-03-25 23:20:13 +0100

    Store Icecast password in Github Secrets

--- push.php
+++ push.php
@@ -1,38 +1,53 @@
 <?php
 
 $metadata = file_get_contents('https://www.zuidwestupdate.nl/rds/rds_programma.php');
-$serversPath = __DIR__ . '/servers.csv';
-if(!file_exists($serversPath)) {
-    error_log('servers.csv doesn\'t exist');
+
+$icecast_password = getenv('ICECAST_PASSWORD');
+if(empty($icecast_password)) {
+    error_log('ICECAST_PASSWORD is not set');
     exit(64);
 }
-$servers = file_get_contents($serversPath);
-$servers = explode("\n", $servers);
 
-//first line are column names;
-array_shift($servers);
+$servers = [
+    [
+        'hostname' => 'icecast.zuidwestfm.nl',
+        'port' => 80,
+        'username' => 'admin',
+        'password' => $icecast_password,
+        'mountpoint' => '/zuidwest.mp3',
+    ],
+    [
+        'hostname' => 'icecast.zuidwestfm.nl',
+        'port' => 80,
+        'username' => 'admin',
+        'password' => $icecast_password,
+        'mountpoint' => '/zuidwest.aac',
+    ],
+    [
+        'hostname' => 'icecast.zuidwestfm.nl',
+        'port' => 80,
+        'username' => 'admin',
+        'password' => $icecast_password,
+        'mountpoint' => '/zuidwest.stl',
+    ],
+];
 
 $failure = false;
 
-foreach($servers as $server) {
-    $server = trim($server);
-    if(empty($server)) {
-        continue;
-    }
-    
-    list($hostName, $port, $username, $password, $mountPoint) = str_getcsv($server, ';');
-    $url = 'https://' . $hostName . ':' . $port . '/admin/metadata.xsl?' . http_build_query(['song' => $metadata, 'mount' => $mountPoint, 'mode' => 'updinfo', 'charset' => 'UTF-8']);
+foreach ($servers as $server) {
+    $url = 'http://' . $server['hostname'] . ':' . $server['port'] . '/admin/metadata.xsl?' . http_build_query(['song' => $metadata, 'mount' => $server['mountpoint'], 'mode' => 'updinfo', 'charset' => 'UTF-8']);
 
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_USERPWD, $username . ':' . $password);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($ch, CURLOPT_USERPWD, $server['username'] . ':' . $server['password']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     if ($responseCode != 200) {
-        error_log(sprintf('FAILURE: %s %s -> %d', $hostName, $mountPoint, $responseCode));
+        error_log(sprintf('FAILURE: %s %s -> %d', $server['hostname'], $server['mountpoint'], $responseCode));
         $failure = true;
-    } else {
-        echo sprintf('SUCCESS: %s %s -> %d', $hostName, $mountPoint, $responseCode) . "\n";
+    } else
+    {
+        echo sprintf('SUCCESS: %s %s -> %d', $server['hostname'], $server['mountpoint'], $responseCode) . "\n";
     }
     curl_close($ch);
 }
