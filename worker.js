9c3eb421fceb70a7cf171dba1af0f26dc42314ba
Raymon Mens <r.mens@me.com>
2024-01-28 00:22:20 +0100

    Fix worker

--- worker.js
+++ worker.js
@@ -1,40 +1,39 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
 const ICECAST_SERVERS = [
-  {hostName: 'icecast.example.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/stream.mp3'},
-  {hostName: 'icecast.example.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/stream.aac'}
+  {hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.mp3'},
+  {hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.aac'}
   // Add more servers if needed
 ]
 
-async function handleRequest(request) {
-  // Fetch current playing song
-  let response = await fetch('https://rds.zuidwestfm.nl/')
-  let song = await response.text()
+export default {
+  async scheduled(event, env, ctx) {
+    // Fetch current playing song
+    let response = await fetch('https://rds.zuidwestfm.nl/')
+    let song = await response.text()
 
-  // Push to Icecast servers
-  let errors = []
-  for(let server of ICECAST_SERVERS){
-    let metadata = {song: song, mount: server.mountPoint, mode: 'updinfo', charset: 'UTF-8'}
-    let requestOptions = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Basic ' + btoa(server.username + ':' + server.password)
+    // Push to Icecast servers
+    let errors = []
+    for(let server of ICECAST_SERVERS){
+      let metadata = {song: song, mount: server.mountPoint, mode: 'updinfo', charset: 'UTF-8'}
+      let requestOptions = {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + btoa(server.username + ':' + server.password)
+        }
+      }
+
+      let url = `http://${server.hostName}:${server.port}/admin/metadata.xsl?` + new URLSearchParams(metadata)
+      let serverResponse = await fetch(url, requestOptions)
+      
+      if (!serverResponse.ok) {
+        errors.push(`Error ${serverResponse.status}: ${serverResponse.statusText} on ${server.hostName}`)
       }
     }
 
-    let url = `http://${server.hostName}:${server.port}/admin/metadata.xsl?` + new URLSearchParams(metadata)
-    let serverResponse = await fetch(url, requestOptions)
-    
-    if (!serverResponse.ok) {
-      errors.push(`Error ${serverResponse.status}: ${serverResponse.statusText} on ${server.hostName}`)
+    if(errors.length > 0){
+      console.error('One or more requests failed: \n' + errors.join('\n'))
+      return;
     }
-  }
 
-  if(errors.length > 0){
-    return new Response('One or more requests failed: \n' + errors.join('\n'), {status: 500})
+    console.log('Updated song metadata')
   }
-
-  return new Response('Updated song metadata', {status: 200})
 }
