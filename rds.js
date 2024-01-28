d0d065e93b38bdf5fa336e92d889fc10e96ec570
rmens <r.mens@me.com>
2024-01-28 13:26:38 +0100

    Refactor

--- rds.js
+++ rds.js
@@ -1,50 +1,48 @@
-addEventListener("fetch", event => {
+addEventListener('fetch', event => {
   event.respondWith(handleRequest(event));
 });
 
 async function handleRequest(event) {
   const request = event.request;
-  const cacheKey = createCacheKey(new URL(request.url));
+  const cacheUrl = createCacheUrl(new URL(request.url));
   const cache = caches.default;
 
-  let response = await cache.match(cacheKey);
-  let cacheStatus = 'Hit';
+  let response = await cache.match(cacheUrl);
+  let headers = new Headers({
+    'Content-Type': 'text/plain; charset=utf-8',
+    'X-Robots-Tag': 'noindex, nofollow, noarchive'
+  });
 
   if (!response) {
-    const fetchResponse = await fetch("https://www.zuidwestupdate.nl/wp-json/zw/v1/broadcast_data");
-    const data = await fetchResponse.json();
+    const data = await fetchBroadcastData();
+    const content = extractContent(new URL(request.url), data);
 
-    const content = new URL(request.url).searchParams.has('ps') ? 
-                    data.fm.rds.program.replace(/<[^>]*>?/gm, '') : 
-                    data.fm.rds.radiotext.replace(/<[^>]*>?/gm, '');
-
-    response = new Response(content, {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/plain; charset=utf-8',
-        'X-Robots-Tag': 'noindex',
-        'X-Cache-Key': cacheKey.url
-      }
-    });
-
-    event.waitUntil(cache.put(cacheKey, response.clone()));
-    cacheStatus = 'Miss';
+    response = new Response(content, { status: 200, headers });
+    event.waitUntil(cache.put(cacheUrl, response.clone()));
+    headers.append('X-Cache-Status', 'Miss');
+  } else {
+    response = new Response(response.body, response);
+    headers = new Headers(response.headers);
+    headers.append('X-Cache-Status', 'Hit');
   }
 
-  // Add 'X-Cache-Status' header to the response
-  const newHeaders = new Headers(response.headers);
-  newHeaders.set('X-Cache-Status', cacheStatus);
-
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: newHeaders
-  });
+  headers.append('X-Cache-Key', cacheUrl.url);
+  return new Response(response.body, { status: response.status, headers });
 }
 
-function createCacheKey(url) {
-  const amsterdamTime = new Date().toLocaleString("en-US", { timeZone: "Europe/Amsterdam" });
+function createCacheUrl(url) {
+  const amsterdamTime = new Date().toLocaleString('en-US', { timeZone: 'Europe/Amsterdam' });
   const formattedTime = new Date(amsterdamTime).toISOString().slice(0, 13).replace(/T/, '-');
   url.searchParams.set('cacheTime', formattedTime);
   return new Request(url.toString(), { method: 'GET' });
+}
+
+async function fetchBroadcastData() {
+  const response = await fetch('https://www.zuidwestupdate.nl/wp-json/zw/v1/broadcast_data');
+  return response.json();
+}
+
+function extractContent(url, data) {
+  const text = url.searchParams.has('ps') ? data.fm.rds.program : data.fm.rds.radiotext;
+  return text.replace(/<[^>]*>?/gm, '');
 }
