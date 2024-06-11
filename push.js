885a929cdfb055f89daa3b04b699fd058e83c80d
rmens <r.mens@me.com>
2024-06-11 22:36:46 +0200

    Sync script from Cloudflare to GitHub

--- push.js
+++ push.js
@@ -1,37 +1,37 @@
 const ICECAST_SERVERS = [
-  { hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.mp3' },
-  { hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.aac' }
+  {hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.mp3'},
+  {hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.aac'}
   // Add more servers if needed
 ]
 
 export default {
-  async scheduled (event, env, ctx) {
+  async scheduled(event, env, ctx) {
     // Fetch current playing song
-    const response = await fetch('https://rds.zuidwestfm.nl/')
-    const song = await response.text()
+    let response = await fetch('https://rds.zuidwestfm.nl/')
+    let song = await response.text()
 
     // Push to Icecast servers
-    const errors = []
-    for (const server of ICECAST_SERVERS) {
-      const metadata = { song, mount: server.mountPoint, mode: 'updinfo', charset: 'UTF-8' }
-      const requestOptions = {
+    let errors = []
+    for(let server of ICECAST_SERVERS){
+      let metadata = {song: song, mount: server.mountPoint, mode: 'updinfo', charset: 'UTF-8'}
+      let requestOptions = {
         method: 'GET',
         headers: {
-          Authorization: 'Basic ' + btoa(server.username + ':' + server.password)
+          'Authorization': 'Basic ' + btoa(server.username + ':' + server.password)
         }
       }
 
-      const url = `http://${server.hostName}:${server.port}/admin/metadata.xsl?` + new URLSearchParams(metadata)
-      const serverResponse = await fetch(url, requestOptions)
-
+      let url = `http://${server.hostName}:${server.port}/admin/metadata.xsl?` + new URLSearchParams(metadata)
+      let serverResponse = await fetch(url, requestOptions)
+      
       if (!serverResponse.ok) {
         errors.push(`Error ${serverResponse.status}: ${serverResponse.statusText} on ${server.hostName}`)
       }
     }
 
-    if (errors.length > 0) {
+    if(errors.length > 0){
       console.error('One or more requests failed: \n' + errors.join('\n'))
-      return
+      return;
     }
 
     console.log('Updated song metadata')
