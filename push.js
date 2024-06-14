09629958e45be8b3359ae3115bd34b88ecb31d93
rmens <r.mens@me.com>
2024-06-15 01:01:26 +0200

    Update push.js

--- push.js
+++ push.js
@@ -2,38 +2,38 @@ const ICECAST_SERVERS = [
   { hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.mp3' },
   { hostName: 'icecast.zuidwestfm.nl', port: 80, username: 'admin', password: 'hackme', mountPoint: '/zuidwest.aac' }
   // Add more servers if needed
-]
+];
 
 export default {
   async scheduled (event, env, ctx) {
     // Fetch current playing song
-    const response = await fetch('https://rds.zuidwestfm.nl/')
-    const song = await response.text()
+    const response = await fetch('https://rds.zuidwestfm.nl/');
+    const song = await response.text();
 
     // Push to Icecast servers
-    let errors = []
-    for (let server of ICECAST_SERVERS){
-      const metadata = { song: song, mount: server.mountPoint, mode: 'updinfo', charset: 'UTF-8' }
+    const errors = [];
+    for (const server of ICECAST_SERVERS) {
+      const metadata = { song, mount: server.mountPoint, mode: 'updinfo', charset: 'UTF-8' };
       const requestOptions = {
         method: 'GET',
         headers: {
           Authorization: 'Basic ' + btoa(server.username + ':' + server.password)
         }
-      }
+      };
 
-      const url = `http://${server.hostName}:${server.port}/admin/metadata.xsl?` + new URLSearchParams(metadata)
-      const serverResponse = await fetch(url, requestOptions)
+      const url = `http://${server.hostName}:${server.port}/admin/metadata.xsl?` + new URLSearchParams(metadata);
+      const serverResponse = await fetch(url, requestOptions);
       
       if (!serverResponse.ok) {
-        errors.push(`Error ${serverResponse.status}: ${serverResponse.statusText} on ${server.hostName}`)
+        errors.push(`Error ${serverResponse.status}: ${serverResponse.statusText} on ${server.hostName}`);
       }
     }
 
-    if(errors.length > 0){
-      console.error('One or more requests failed: \n' + errors.join('\n'))
+    if (errors.length > 0) {
+      console.error('One or more requests failed: \n' + errors.join('\n'));
       return;
     }
 
-    console.log('Updated song metadata')
+    console.log('Updated song metadata');
   }
-}
+};
